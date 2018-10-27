11333743d8bb7a5d1add2ead92209e963b4adb49
Ben <joelbenjamin093@gmail.com>
2018-10-27 16:07:55 +0530

    got the thing to work

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ $("body").append(`
 
 `)
 
-console.log("loaded");
+/*
 
-let txt_area = document.querySelectorAll(".rta__textarea");
-let search_field = document.querySelector("#gk_search_bar");
-let xbtn = document.querySelector("#gk_popup_close");
+  # Gif popup properties
 
 
+  hide: hides the popup
+  show: Displays the popup near the current mouse position
+  ClearGifBox : Clears all the images inside the gif box if present
+  MakeGif : Requests for gif images and adds them to the gif box
 
+*/
 function GifPopupProperties(){
 
   let gifPopup = document.querySelector("#gk_popup");
@@ -33,14 +36,14 @@ function GifPopupProperties(){
   },
 
   this.show = function(textbox){
-    console.log("lalala");
+
     currentTextBox = textbox;
 
     let textboxCount = currentTextBox.value.split(" ");
     let last_element = textboxCount[textboxCount.length - 1];
     textboxCount = [];
     let caret = getCaretCoordinates(currentTextBox, currentTextBox.selectionEnd);
-    console.log(caret);
+
     if (last_element == "$:") {
       console.log("NICE!");
 
@@ -62,9 +65,11 @@ function GifPopupProperties(){
   this.MakeGif = function(url){
     let img_ele = document.createElement("img");
     img_ele.src = url;
-    img_ele.classList.add("gk_gif_images")
-    img_ele.onclick = function(event){
+    img_ele.classList.add("gk_gif_images");
+    console.log(img_ele);
+    img_ele.onclick = function(e){
       // add the url to the text box
+      console.log(currentTextBox);
       currentTextBox.value = currentTextBox.value.replace("$:",url);
       gifPopup.style.display = "none";
     }
@@ -75,11 +80,41 @@ function GifPopupProperties(){
 
 let gifKittyBox = new GifPopupProperties();
 
+let mutationObserver = new MutationObserver(function(mutations) {
+
+  let all_txt = document.querySelectorAll(".rta__textarea");
+
+  all_txt.forEach(function(current_box){
+    current_box.addEventListener("keyup", function(e){
+        gifKittyBox.show(current_box)
+    });
+  })
+
+  console.log("DOM has changed!");
+});
+
+// mutation observer configs
+mutationObserver.observe(document.documentElement, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+
+
+let txt_area = document.querySelectorAll(".rta__textarea");
+let search_field = document.querySelector("#gk_search_bar");
+let xbtn = document.querySelector(".gk_xbtn");
+
+
+// for when the page is static and doesn't have any dom changes
 for (let count=0; count < txt_area.length; count++) {
   let current_box = txt_area[count];
+  console.log("OK");
 
   current_box.addEventListener("keyup", function(e){
-      console.log("event added");
       gifKittyBox.show(current_box)
 
   });
@@ -112,16 +147,33 @@ xbtn.addEventListener("click",function(e){
 // search function
 function search_for(gif_endpoint){
 
-  fetch(gif_endpoint)
-  .then(data => data.json())
-  .then(function(gif_data){
-    for (let gif_count in gif_data) {
-      console.log(gif_data[gif_count]);
-      let len_gifar = gif_data[gif_count].length;
-      for (let i = 0; i < len_gifar; i++) {
-        let img_url = gif_data[gif_count][i]["images"]["preview_gif"]["url"];
-        gifKittyBox.MakeGif(img_url);
-      }
+  $.ajax({
+    url : gif_endpoint,
+    type: "GET",
+    success: function(data){
+      //console.log(data);
+      generateGifs(data);
     }
   })
+
+  // fetch(gif_endpoint)
+  // .then(data => data.json())
+  // .then(function(gif_data){
+  //
+  //   generateGifs(gif_data)
+  //
+  // })
+}
+
+function generateGifs(gif_data){
+
+  for (let gif_count in gif_data) {
+    // console.log(gif_data[gif_count]);
+    let len_gifar = gif_data[gif_count].length;
+    for (let i = 0; i < len_gifar; i++) {
+      let img_url = gif_data[gif_count][i]["images"]["preview_gif"]["url"];
+      gifKittyBox.MakeGif(img_url);
+    }
+  }
+
 }
